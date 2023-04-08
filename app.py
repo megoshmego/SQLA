3cc74f49710950c929d83d6e63088188f5971b2b
megoshmego <108840586+megoshmego@users.noreply.github.com>
2023-04-08 13:42:35 -0400

    +

--- app.py
+++ app.py
@@ -91,3 +91,5 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
 
+
+
--- practice_mult_db/models.py
+++ practice_mult_db/models.py
@@ -7,3 +7,24 @@ def connect_db(app):
     db.init_app(app)
 
 
+class Department(db.Model):
+    """Department. A department has many empoloyees"""
+
+    __tablename__ = "departments"
+
+    dept_code = db.Column(db.Text, primary_key=True)
+    dept_name = db.Column(db.Text,
+                        nullabble=False,
+                        unique=True)
+    phone = db.Column(db.Text)
+
+
+class Employee(db.Model):
+    """Employee Model"""
+
+    __tablename__ = "employees"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    state = db.Column(db.Text, nullable=False, default='CA')
+    
